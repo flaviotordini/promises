e27ae5d278b68a7a154408a8be9a707aa3442e08
Flavio Tordini <flavio.tordini@gmail.com>
2023-08-28 22:59:01 +0200

    onFailed and finally return Promise<T>

--- promise.h
+++ promise.h
@@ -8,19 +8,7 @@ class BasePromise : public QObject {
     Q_OBJECT
 
 public:
-    explicit BasePromise(QObject *parent = nullptr) : QObject(parent) {
-        connect(this, &BasePromise::resolve, this, &QObject::deleteLater);
-        connect(this, &BasePromise::reject, this, &QObject::deleteLater);
-    }
-
-    template <typename Function> BasePromise &onFailed(Function func) {
-        connect(this, &BasePromise::reject, this, func);
-        return *this;
-    }
-    template <typename Function> BasePromise &finally(Function func) {
-        connect(this, &BasePromise::destroyed, this, func);
-        return *this;
-    }
+    explicit BasePromise(QObject *parent = nullptr) : QObject(parent) {}
 
 signals:
     void resolve();
@@ -29,10 +17,23 @@ signals:
 
 template <class T> class Promise : public BasePromise {
 public:
-    explicit Promise(QObject *parent = nullptr) : BasePromise(parent) {}
+    explicit Promise(QObject *parent = nullptr) : BasePromise(parent) {
+        connect(this, &BasePromise::resolve, this, &QObject::deleteLater);
+        connect(this, &BasePromise::reject, this, &QObject::deleteLater);
+    }
 
-    template <typename Function> BasePromise &then(Function func) {
+    template <typename Function> Promise<T> &then(Function func) {
         connect(this, &BasePromise::resolve, this, [this, func] { func(data); });
+        return *this;
+    }
+
+    template <typename Function> Promise<T> &onFailed(Function func) {
+        connect(this, &BasePromise::reject, this, func);
+        return *this;
+    }
+
+    template <typename Function> Promise<T> &finally(Function func) {
+        connect(this, &BasePromise::destroyed, this, func);
         return *this;
     }
 
@@ -40,6 +41,7 @@ public:
         data = value;
         BasePromise::resolve();
     }
+
     T result() const { return data; }
 
 private:
